fc17f4bf4d08814fc21d998ee1c25d5b16e84959
Walter <walterxie@users.noreply.github.com>
2025-09-02 11:08:00 +1200

    correct Simplex and StochasticMatrix #3

--- core/java/src/main/java/org/phylospec/types/StochasticMatrix.java
+++ core/java/src/main/java/org/phylospec/types/StochasticMatrix.java
@@ -24,7 +24,7 @@ import org.phylospec.primitives.UnitInterval;
  * @author PhyloSpec Contributors
  * @since 1.0
  */
-public interface StochasticMatrix<P extends UnitInterval> extends Matrix<P> {
+public interface StochasticMatrix extends Matrix<UnitInterval> {
 
     /**
      * {@inheritDoc}
