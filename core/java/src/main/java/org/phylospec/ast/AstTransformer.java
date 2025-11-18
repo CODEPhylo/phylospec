6f85b703c3f3e6e0e196a3bbc009c8b91209abe5
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-11-18 10:19:35 +0100

    Add basic AST transformation framework.

---
+++ core/java/src/main/java/org/phylospec/ast/AstTransformer.java
@@ -0,0 +1,122 @@
+package org.phylospec.ast;
+
+import org.phylospec.components.ComponentResolver;
+
+import java.util.List;
+
+/**
+ * This interface can be implemented by classes that transform the AST tree.
+ * The transformation should be stateless to be correct.
+ */
+public abstract class AstTransformer<S, E, T> implements AstVisitor<Stmt, Expr, AstType> {
+    List<Stmt> oldStatements;
+    List<Stmt> transformedStatements;
+
+    public List<Stmt> transformStatements(List<Stmt> statements, ComponentResolver componentResolver) {
+        oldStatements = statements;
+
+        for (Stmt oldStatement : statements) {
+            oldStatement.accept(this);
+        }
+
+        return transformedStatements;
+    }
+
+    @Override
+    public Stmt visitDecoratedStmt(Stmt.Decorated stmt) {
+        stmt.statememt.accept(this);
+        return stmt;
+    }
+
+    @Override
+    public Stmt visitAssignment(Stmt.Assignment stmt) {
+        stmt.expression.accept(this);
+        return stmt;
+    }
+
+    @Override
+    public Stmt visitDraw(Stmt.Draw stmt) {
+        stmt.expression.accept(this);
+        return stmt;
+    }
+
+    @Override
+    public Stmt visitImport(Stmt.Import stmt) {
+        return stmt;
+    }
+
+    @Override
+    public Expr visitLiteral(Expr.Literal expr) {
+        return expr;
+    }
+
+    @Override
+    public Expr visitVariable(Expr.Variable expr) {
+        return expr;
+    }
+
+    @Override
+    public Expr visitUnary(Expr.Unary expr) {
+        expr.right.accept(this);
+        return expr;
+    }
+
+    @Override
+    public Expr visitBinary(Expr.Binary expr) {
+        expr.left.accept(this);
+        expr.right.accept(this);
+        return expr;
+    }
+
+    @Override
+    public Expr visitCall(Expr.Call expr) {
+        for (Expr.Argument argument : expr.arguments) {
+            argument.accept(this);
+        }
+        return expr;
+    }
+
+    @Override
+    public Expr visitAssignedArgument(Expr.AssignedArgument expr) {
+        expr.expression.accept(this);
+        return expr;
+    }
+
+    @Override
+    public Expr visitDrawnArgument(Expr.DrawnArgument expr) {
+        expr.expression.accept(this);
+        return expr;
+    }
+
+    @Override
+    public Expr visitGrouping(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public Expr visitArray(Expr.Array expr) {
+        for (Expr element : expr.elements) {
+            element.accept(this);
+        }
+        return expr;
+    }
+
+    @Override
+    public Expr visitGet(Expr.Get expr) {
+        expr.object.accept(this);
+        return expr;
+    }
+
+    @Override
+    public AstType visitAtomicType(AstType.Atomic expr) {
+        return expr;
+    }
+
+    @Override
+    public AstType visitGenericType(AstType.Generic expr) {
+        for (AstType type : expr.typeParameters) {
+            type.accept(this);
+        }
+        return expr;
+    }
+}
---
+++ core/java/src/main/java/org/phylospec/ast/transformers/IIDTransformer.java
@@ -0,0 +1,16 @@
+package org.phylospec.ast.transformers;
+
+import org.phylospec.ast.AstTransformer;
+import org.phylospec.ast.AstType;
+import org.phylospec.ast.Expr;
+import org.phylospec.ast.Stmt;
+import org.phylospec.components.ComponentResolver;
+
+import java.util.List;
+
+/**
+ * This class takes a syntax tree and desugarifies the IID distributions into
+ */
+public class IIDTransformer extends AstTransformer {
+
+}
