47f89b0290f359c473bc8c0f15a4bcfd4e9fbd41
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-11-18 10:46:49 +0100

    Add list comprehension tests.

--- core/java/src/main/java/org/phylospec/lexer/TokenType.java
+++ core/java/src/main/java/org/phylospec/lexer/TokenType.java
@@ -3,7 +3,7 @@ package org.phylospec.lexer;
 public enum TokenType {
     // single-character tokens
     LEFT_PAREN, RIGHT_PAREN, COMMA, DOT, MINUS, PLUS,
-    SLASH, STAR, TILDE, AT, LEFT_SQUARE_BRACKET, RIGHT_SQUARE_BRACKET,
+    SLASH, STAR, TILDE, AT, LEFT_SQUARE_BRACKET, RIGHT_SQUARE_BRACKET, FOR,
 
     // one or two character tokens
     BANG, BANG_EQUAL,
