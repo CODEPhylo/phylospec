feff4241bbc57feb66638657f05aee019af1c80b
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-11-22 22:27:20 +0100

    Create website scaffold.

---
+++ website/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="bg-background font-sans text-dark min-h-lvh w-lvw flex flex-col items-center">
+	<header class="w-full bg-light-gray p-4">
+		<a class="text-2xl font-bold text-gray-800" href="/">
+			<span class="text-accent">Phylo</span>Spec
+		</a>
+	</header>
+
+	{@render children()}
+</div>
---
+++ website/svelte.config.js
@@ -0,0 +1,30 @@
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import adapter from '@sveltejs/adapter-auto';
+import { createHighlighter } from 'shiki'
+import fs from 'fs';
+
+const theme = JSON.parse(fs.readFileSync('src/lib/themes/nord.json', 'utf8'));
+const phylospec = JSON.parse(fs.readFileSync('src/lib/phylospec.json', 'utf8'))
+const highlighter = await createHighlighter({
+	themes: [theme],
+	langs: [phylospec, 'javascript', 'json', 'java', 'bash', 'bibtex'],
+})
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: [mdsvex({
+		extensions: ['.md'],
+		highlight: {
+			highlighter: async (code, lang = 'text') => {
+				const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
+				return `{@html \`${html}\` }`;
+			}
+		},
+	})],
+	extensions: ['.svelte', '.svx', '.md']
+};
+
+export default config;
