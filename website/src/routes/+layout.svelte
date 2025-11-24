110f6aada3d18d37a34253a38914d81bb1616ea7
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-11-24 16:11:06 +0100

    Add border to header.

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 
 <div class="bg-background font-sans text-dark min-h-lvh w-lvw flex flex-col items-center pt-16">
 	<header
-		class="w-full bg-light-gray/80 backdrop-blur-md p-4 flex items-center fixed top-0 left-0 right-0 z-10"
+		class="w-full bg-light-gray/80 backdrop-blur-md p-4 flex items-center fixed top-0 left-0 right-0 z-10 border-b border-gray-200"
 	>
 		<a class="text-2xl font-bold flex-1" href="/">
 			<span class="text-accent">Phylo</span><span class="text-gray-900">Spec</span>
