bf445de0bb0ace0db4dcef0570205fcb7e9c1822
Tobia Ochsner <tobia.ochsner@hotmail.com>
2026-04-09 16:46:54 +0200

    Add date parsing.

--- integrations/beast3/java/src/main/java/tiles/input/FromNexusTile.java
+++ integrations/beast3/java/src/main/java/tiles/input/FromNexusTile.java
@@ -20,6 +20,7 @@ public class FromNexusTile extends GeneratorTile<DecoratedAlignment> {
 
     TileInput<String> fileInput = new TileInput<>("file");
     TileInput<ParserTile.Parser> ageInput = new TileInput<>("age", false);
+    TileInput<ParserTile.Parser> dateInput = new TileInput<>("date", false);
 
     @Override
     public DecoratedAlignment applyTile(BEASTState beastState) {
@@ -42,8 +43,10 @@ public class FromNexusTile extends GeneratorTile<DecoratedAlignment> {
 
         // build age trait set if needed
 
-        TraitSet ageTraitSet = null;
         ParserTile.Parser ageParser = this.ageInput.apply(beastState);
+        ParserTile.Parser dateParser = this.dateInput.apply(beastState);
+
+        TraitSet ageTraitSet = null;
         if (ageParser != null) {
             ageTraitSet = new TraitSet();
 
@@ -53,6 +56,19 @@ public class FromNexusTile extends GeneratorTile<DecoratedAlignment> {
             }
 
             beastState.setInput(ageTraitSet, ageTraitSet.traitNameInput, "age");
+            beastState.setInput(ageTraitSet, ageTraitSet.taxaInput, taxonSet);
+            beastState.setInput(ageTraitSet, ageTraitSet.traitsInput, traits.toString());
+
+            ageTraitSet.initAndValidate();
+        } else if (dateParser != null) {
+            ageTraitSet = new TraitSet();
+
+            StringBuilder traits = new StringBuilder();
+            for (String taxon : alignment.getTaxaNames()) {
+                traits.append(taxon).append("=").append(ageParser.parse(taxon)).append(",");
+            }
+
+            beastState.setInput(ageTraitSet, ageTraitSet.traitNameInput, "date");
             beastState.setInput(ageTraitSet, ageTraitSet.taxaInput, taxonSet);
             beastState.setInput(ageTraitSet, ageTraitSet.traitsInput, traits.toString());
 
