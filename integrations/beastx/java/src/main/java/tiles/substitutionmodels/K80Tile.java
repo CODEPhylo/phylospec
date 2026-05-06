20804c3d7c170140363d45c1723f0d647dd4c586
Haoyuan Huang <hhua361@aucklanduni.ac.nz>
2026-05-06 20:22:49 +1200

    Add BEAST X K80 substitution model tile

---
+++ integrations/beastx/java/src/main/java/tiles/substitutionmodels/K80Tile.java
@@ -0,0 +1,36 @@
+package tiles.substitutionmodels;
+
+import dr.evolution.datatype.Nucleotides;
+import dr.evomodel.substmodel.FrequencyModel;
+import dr.evomodel.substmodel.nucleotide.HKY;
+import org.phylospec.ast.Expr;
+import org.phylospec.domain.PositiveReal;
+import org.phylospec.tiling.tiles.GeneratorTile;
+import org.phylospec.types.RealScalar;
+import tiling.BeastXState;
+
+import java.util.IdentityHashMap;
+
+public class K80Tile extends GeneratorTile<HKY, BeastXState> {
+
+    @Override
+    public String getPhyloSpecGeneratorName() {
+        return "k80";
+    }
+
+    GeneratorTileInput<RealScalar<PositiveReal>, BeastXState> kappaInput =
+            new GeneratorTileInput<>("kappa");
+
+    @Override
+    public HKY applyTile(BeastXState beastState, IdentityHashMap<Expr.Variable, Integer> indexVariables) {
+        RealScalar<PositiveReal> kappaScalar = this.kappaInput.apply(beastState, indexVariables);
+        double kappa = kappaScalar.get();
+
+        FrequencyModel frequencies = new FrequencyModel(
+                Nucleotides.INSTANCE,
+                new double[]{0.25, 0.25, 0.25, 0.25}
+        );
+
+        return new HKY(kappa, frequencies);
+    }
+}
