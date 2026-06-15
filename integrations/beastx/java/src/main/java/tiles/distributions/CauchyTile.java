52205bc5ee5a1965a23576313d72af6657e612bc
Haoyuan Huang <hhua361@aucklanduni.ac.nz>
2026-06-15 22:43:00 +1200

    PhyloSpec Beast X File Cleanup

--- integrations/beastx/java/src/main/java/tiles/distributions/CauchyTile.java
+++ integrations/beastx/java/src/main/java/tiles/distributions/CauchyTile.java
@@ -60,4 +60,3 @@ public class CauchyTile extends GeneratorTile<
         );
     }
 }
-
--- integrations/beastx/java/src/test/java/BeastXXmlProteinTraitPhyloCTMCTest.java
+++ integrations/beastx/java/src/test/java/BeastXXmlProteinTraitPhyloCTMCTest.java
@@ -41,47 +41,47 @@ public class BeastXXmlProteinTraitPhyloCTMCTest {
         String source =
                 """
                 Alignment data = fromNexus("src/test/java/resources/protein-simple.nex")
-    
+
                 Taxa taxa = taxa(data)
-    
+
                 PositiveReal birthRate ~ LogNormal(
                     logMean=0.0,
                     logSd=1.0
                 )
-    
+
                 PositiveReal clockRate ~ LogNormal(
                     logMean=0.0,
                     logSd=1.0
                 )
-    
+
                 Tree tree ~ Yule(
                     birthRate=birthRate,
                     taxa=taxa
                 )
-    
+
                 Vector<Rate> branchRates ~ StrictClock(
                     clockRate=clockRate,
                     tree=tree
                 )
-    
+
                 QMatrix q = jtt()
-    
+
                 Alignment alignment ~ PhyloCTMC(
                     tree=tree,
                     qMatrix=q,
                     branchRates=branchRates
                 ) observed as data
-    
+
                 mcmc {
                     Integer chainLength = 10000
                     Integer randomSeed = 1234
-    
+
                     Logger fileLogger = fileLogger(
                         logEvery=1,
                         file="%s",
                         parameters=[birthRate, clockRate]
                     )
-    
+
                     Logger treeLogger = treeLogger(
                         logEvery=1,
                         file="%s",
@@ -136,71 +136,71 @@ public class BeastXXmlProteinTraitPhyloCTMCTest {
         String source =
                 """
                 Alignment fullData = fromNexus("src/test/java/resources/primate-mtDNA.nex")
-    
+
                 Alignment codonData = subset(
                     alignment=fullData,
                     start=1,
                     end=600
                 )
-    
+
                 Taxa taxa = taxa(codonData)
-    
+
                 PositiveReal birthRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 PositiveReal clockRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 PositiveReal kappa ~ LogNormal(
                     logMean=1.0,
                     logSd=0.4
                 )
-    
+
                 PositiveReal omega ~ LogNormal(
                     logMean=-0.5,
                     logSd=0.5
                 )
-    
+
                 Simplex codonFrequencies ~ Dirichlet(
                     concentration=repeat(1.0, num=61)
                 )
-    
+
                 Tree tree ~ Yule(
                     birthRate=birthRate,
                     taxa=taxa
                 )
-    
+
                 Vector<Rate> branchRates ~ StrictClock(
                     clockRate=clockRate,
                     tree=tree
                 )
-    
+
                 QMatrix q = gy94(
                     kappa=kappa,
                     omega=omega,
                     baseFrequencies=codonFrequencies
                 )
-    
+
                 Alignment codonAlignment ~ PhyloCTMC(
                     tree=tree,
                     qMatrix=q,
                     branchRates=branchRates
                 ) observed as codonData
-    
+
                 mcmc {
                     Integer chainLength = 5
                     Integer randomSeed = 1234
-    
+
                     Logger fileLogger = fileLogger(
                         logEvery=1,
                         file="target/beastx-xml-execution/gy94CodonPhyloCTMC.log",
                         parameters=[birthRate, clockRate, kappa, omega]
                     )
-    
+
                     Logger treeLogger = treeLogger(
                         logEvery=1,
                         file="target/beastx-xml-execution/gy94CodonPhyloCTMC.trees",
@@ -254,59 +254,59 @@ public class BeastXXmlProteinTraitPhyloCTMCTest {
         String source =
                 """
                 Alignment molecularData = fromNexus("src/test/java/resources/binary-traits.nex")
-    
+
                 Taxa taxa = taxa(molecularData)
-    
+
                 Alignment traitData = discreteTraitsFromTaxa(
                     taxa=taxa,
                     trait=parse(regex=".*_([01])$")
                 )
-    
+
                 PositiveReal birthRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 PositiveReal clockRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 Rate traitRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 Tree tree ~ Yule(
                     birthRate=birthRate,
                     taxa=taxa
                 )
-    
+
                 Vector<Rate> branchRates ~ StrictClock(
                     clockRate=clockRate,
                     tree=tree
                 )
-    
+
                 QMatrix traitQ = mk(
                     rate=traitRate
                 )
-    
+
                 Alignment traitAlignment ~ PhyloCTMC(
                     tree=tree,
                     qMatrix=traitQ,
                     branchRates=branchRates
                 ) observed as traitData
-    
+
                 mcmc {
                     Integer chainLength = 10000
                     Integer randomSeed = 1234
-    
+
                     Logger fileLogger = fileLogger(
                         logEvery=1,
                         file="%s",
                         parameters=[birthRate, clockRate, traitRate]
                     )
-    
+
                     Logger treeLogger = treeLogger(
                         logEvery=1,
                         file="%s",
@@ -374,79 +374,79 @@ public class BeastXXmlProteinTraitPhyloCTMCTest {
         String source =
                 """
                 Alignment molecularData = fromNexus("src/test/java/resources/binary-traits.nex")
-    
+
                 Taxa taxa = taxa(molecularData)
-    
+
                 Alignment traitData = discreteTraitsFromTaxa(
                     taxa=taxa,
                     trait=parse(regex=".*_([01])$")
                 )
-    
+
                 Rate birthRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 Rate clockRate ~ LogNormal(
                     logMean=0.0,
                     logSd=0.5
                 )
-    
+
                 PositiveReal kappa ~ LogNormal(
                     logMean=1.0,
                     logSd=0.4
                 )
-    
+
                 Simplex baseFrequencies ~ Dirichlet(
                     concentration=repeat(1.0, num=4)
                 )
-    
+
                 Rate traitRate ~ LogNormal(
                     logMean=-1.0,
                     logSd=0.5
                 )
-    
+
                 Tree tree ~ Yule(
                     birthRate=birthRate,
                     taxa=taxa
                 )
-    
+
                 Vector<Rate> branchRates ~ StrictClock(
                     clockRate=clockRate,
                     tree=tree
                 )
-    
+
                 QMatrix molecularQ = hky(
                     kappa=kappa,
                     baseFrequencies=baseFrequencies
                 )
-    
+
                 QMatrix traitQ = mk(
                     rate=traitRate
                 )
-    
+
                 Alignment molecularAlignment ~ PhyloCTMC(
                     tree=tree,
                     qMatrix=molecularQ,
                     branchRates=branchRates
                 ) observed as molecularData
-    
+
                 Alignment traitAlignment ~ PhyloCTMC(
                     tree=tree,
                     qMatrix=traitQ,
                     branchRates=branchRates
                 ) observed as traitData
-    
+
                 mcmc {
                     Integer chainLength = 1000
                     Integer randomSeed = 1234
-    
+
                     Logger fileLogger = fileLogger(
                         logEvery=1,
                         file="%s",
                         parameters=[birthRate, clockRate, kappa, traitRate]
                     )
-    
+
                     Logger treeLogger = treeLogger(
                         logEvery=1,
                         file="%s",
@@ -504,5 +504,3 @@ public class BeastXXmlProteinTraitPhyloCTMCTest {
     }
 
 }
-
-
