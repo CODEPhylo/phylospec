f7a60c959c05c6965871e422737d4b3e5e6af1c0
Tobia Ochsner <tobia.ochsner@hotmail.com>
2026-08-15 14:58:10 +0200

    Refactor tiling loading logic.

--- integrations/beast3/java/src/main/java/Test2.java
+++ integrations/beast3/java/src/main/java/Test2.java
@@ -31,7 +31,7 @@ public class Test2 {
            }
         """;
 
-        PhyloSpecRunner parser = new PhyloSpecRunner(source);
+        PhyloSpecRunner<?, ?> parser = new Beast3Runner(source);
         parser.runPhyloSpec("Test2");
     }
 
