3da6ffe06c392e376e8b19fc8394cd755c3c8f6b
Tobia Ochsner <tobia.ochsner@hotmail.com>
2026-08-18 21:40:20 +0200

    Remove named logged paramters due to missing support.

--- core/java/src/main/java/org/phylospec/tiling/mcmc/FileLoggerSpec.java
+++ core/java/src/main/java/org/phylospec/tiling/mcmc/FileLoggerSpec.java
@@ -1,7 +1,5 @@
 package org.phylospec.tiling.mcmc;
 
 import java.util.List;
-import java.util.Map;
 
-public record FileLoggerSpec<T>(
-        int logEvery, String fileName, List<T> parameters, Map<String, T> namedParameters) {}
+public record FileLoggerSpec<T>(int logEvery, String fileName, List<T> parameters) {}
