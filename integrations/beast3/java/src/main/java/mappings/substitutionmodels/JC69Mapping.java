14c256d540acee444ee3943c8797220552fa6284
hhua361 <128016433+hhua361@users.noreply.github.com>
2026-09-01 09:43:33 +1200

    Declare JC69 generator mapping

---
+++ integrations/beast3/java/src/main/java/mappings/substitutionmodels/JC69Mapping.java
@@ -0,0 +1,9 @@
+package mappings.substitutionmodels;
+
+import beast.base.spec.evolution.substitutionmodel.JukesCantor;
+import org.phylospec.annotations.GeneratorMapping;
+
+@GeneratorMapping(
+        component = "phylospec.functions.substitution.jc69",
+        implementation = JukesCantor.class)
+public interface JC69Mapping {}
