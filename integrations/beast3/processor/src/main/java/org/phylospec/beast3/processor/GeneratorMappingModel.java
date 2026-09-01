a73243b788fe8e35f590ce0718b1afe2dd3bff71
hhua361 <128016433+hhua361@users.noreply.github.com>
2026-09-01 12:02:05 +1200

    Parse and validate generator mappings

---
+++ integrations/beast3/processor/src/main/java/org/phylospec/beast3/processor/GeneratorMappingModel.java
@@ -0,0 +1,11 @@
+package org.phylospec.beast3.processor;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+
+record GeneratorMappingModel(
+        TypeElement declaration,
+        String qualifiedComponentName,
+        String namespace,
+        String componentName,
+        TypeMirror implementationType) {}
--- integrations/beast3/processor/src/main/java/org/phylospec/beast3/processor/GeneratorMappingProcessor.java
+++ integrations/beast3/processor/src/main/java/org/phylospec/beast3/processor/GeneratorMappingProcessor.java
@@ -1,5 +1,13 @@
 package org.phylospec.beast3.processor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
@@ -41,15 +49,130 @@ public final class GeneratorMappingProcessor extends AbstractProcessor {
                 continue;
             }
 
-            processingEnv
-                    .getMessager()
-                    .printMessage(
-                            Diagnostic.Kind.NOTE,
-                            "Discovered generator mapping: "
-                                    + element,
-                            element);
+            TypeElement mappingDeclaration = (TypeElement) element;
+
+            readMapping(mappingDeclaration)
+                    .ifPresent(
+                            mapping ->
+                                    processingEnv
+                                            .getMessager()
+                                            .printMessage(
+                                                    Diagnostic.Kind.NOTE,
+                                                    "Discovered generator mapping: "
+                                                            + mapping.qualifiedComponentName()
+                                                            + " -> "
+                                                            + mapping.implementationType(),
+                                                    mapping.declaration()));
         }
 
         return true;
+    }
+
+    private Optional<GeneratorMappingModel> readMapping(TypeElement declaration) {
+        AnnotationMirror mappingAnnotation =
+                declaration.getAnnotationMirrors().stream()
+                        .filter(this::isGeneratorMapping)
+                        .findFirst()
+                        .orElseThrow(
+                                () ->
+                                        new IllegalStateException(
+                                                "Expected @GeneratorMapping on "
+                                                        + declaration.getQualifiedName()));
+
+        Map<String, AnnotationValue> values = new HashMap<>();
+
+        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry :
+                processingEnv
+                        .getElementUtils()
+                        .getElementValuesWithDefaults(mappingAnnotation)
+                        .entrySet()) {
+
+            values.put(
+                    entry.getKey().getSimpleName().toString(),
+                    entry.getValue());
+        }
+
+        String qualifiedComponentName =
+                (String) values.get("component").getValue();
+
+        TypeMirror implementationType =
+                (TypeMirror) values.get("implementation").getValue();
+
+        if (qualifiedComponentName.isBlank()) {
+            printError(
+                    "@GeneratorMapping component must not be blank.",
+                    declaration);
+            return Optional.empty();
+        }
+
+        int finalSeparator = qualifiedComponentName.lastIndexOf('.');
+
+        if (finalSeparator <= 0
+                || finalSeparator == qualifiedComponentName.length() - 1) {
+            printError(
+                    "@GeneratorMapping component must be fully qualified, for example "
+                            + "'phylospec.functions.substitution.jc69'.",
+                    declaration);
+            return Optional.empty();
+        }
+
+        String namespace =
+                qualifiedComponentName.substring(0, finalSeparator);
+
+        String componentName =
+                qualifiedComponentName.substring(finalSeparator + 1);
+
+        Element implementationElement =
+                processingEnv
+                        .getTypeUtils()
+                        .asElement(implementationType);
+
+        if (!(implementationElement
+                instanceof TypeElement implementationDeclaration)
+                || implementationDeclaration.getKind() != ElementKind.CLASS) {
+
+            printError(
+                    "@GeneratorMapping implementation must refer to a class.",
+                    declaration);
+            return Optional.empty();
+        }
+
+        if (implementationDeclaration
+                .getModifiers()
+                .contains(Modifier.ABSTRACT)) {
+
+            printError(
+                    "@GeneratorMapping implementation must not be abstract.",
+                    declaration);
+            return Optional.empty();
+        }
+
+        return Optional.of(
+                new GeneratorMappingModel(
+                        declaration,
+                        qualifiedComponentName,
+                        namespace,
+                        componentName,
+                        implementationType));
+    }
+
+    private boolean isGeneratorMapping(AnnotationMirror annotation) {
+        Element annotationElement =
+                annotation.getAnnotationType().asElement();
+
+        return annotationElement instanceof TypeElement annotationType
+                && annotationType
+                .getQualifiedName()
+                .contentEquals(
+                        GeneratorMapping.class.getCanonicalName());
+    }
+
+    private void printError(String message, Element element) {
+        processingEnv
+                .getMessager()
+                .printMessage(
+                        Diagnostic.Kind.ERROR,
+                        message,
+                        element);
     }
 }
